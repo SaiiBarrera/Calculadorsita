099f4afffc26ada477e20b4ea7970bc511db08ee
SaiiBarrera <saidy_chivas_92@hotmail.com>
2016-10-03 15:48:33 -0500

    ok
    doki

--- CalculadoraConversora/app/src/main/java/com/example/oithanathozio/calculadoraconversora/MainActivity.java
+++ CalculadoraConversora/app/src/main/java/com/example/oithanathozio/calculadoraconversora/MainActivity.java
@@ -4,7 +4,7 @@ import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 
 public class MainActivity extends AppCompatActivity {
-//temito el cangurito :P  
+//temito el cangurito :P
 
 
     @Override
